8489b8f24cba2eb820cacf0b89a3a61b996f185f
yehezkiel <yehezkieled@gmail.com>
2025-02-17 23:12:30 +1100

    add project page and fix some issue

--- website/views.py
+++ website/views.py
@@ -6,3 +6,14 @@ views = Blueprint("views", __name__)
 def home():
     return render_template("home.html")
 
+@views.route("/gomoku")
+def gomoku():
+    return render_template("gomoku.html")
+
+@views.route("/project2")
+def project2():
+    return render_template("project2.html")
+
+@views.route("/project3")
+def project3():
+    return render_template("project3.html")
